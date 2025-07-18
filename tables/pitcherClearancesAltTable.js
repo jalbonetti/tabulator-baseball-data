c9fab1f412a86c16b262ff52c7ca51f56bb30b2c
jalbonetti <jpalbonetti@gmail.com>
2025-07-17 21:35:19 -0400

    Update pitcherClearancesAltTable.js

--- tables/pitcherClearancesAltTable.js
+++ tables/pitcherClearancesAltTable.js
@@ -90,8 +90,8 @@ export class PitcherClearancesAltTable extends BaseTable {
                 {
                     title: "Prop", 
                     field: "Pitcher Prop Type", 
-                    width: 140, 
-                    minWidth: 100,
+                    width: 200, 
+                    minWidth: 150,
                     sorter: "string", 
                     headerFilter: createCustomMultiSelect,  // Use multiselect
                     resizable: false
@@ -99,8 +99,8 @@ export class PitcherClearancesAltTable extends BaseTable {
                 {
                     title: "Value", 
                     field: "Pitcher Prop Value", 
-                    width: 90, 
-                    minWidth: 70,
+                    width: 200, 
+                    minWidth: 150,
                     sorter: "number", 
                     headerFilter: createCustomMultiSelect,  // Use multiselect
                     resizable: false
